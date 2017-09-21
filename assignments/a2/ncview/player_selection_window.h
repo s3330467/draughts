3c05f12b649d6ee2078eb44ecb90d0abf7a9134d
1wsx10 <1wsx10@gmail.com>
2017-09-21 19:50:44 +1000

    put some spacing in his header for readability

--- assignments/a2/ncview/player_selection_window.h
+++ assignments/a2/ncview/player_selection_window.h
@@ -16,20 +16,26 @@ namespace draughts
     }
     namespace ncview
     {
+		//added some spacing between different things to promote readability
         using player_map = std::map<int, std::string>;
         const int NUM_PLAYERS = 2;
+
+
         class player_selection_window : public window
         {
             const std::map<int,std::string> full_list;
             std::map<int,std::string> selected_list;
+
             public:
             player_selection_window(const player_map&);
+
             virtual void activate(void) override;
             std::string players_to_string(void);
             static std::vector<std::string> player_strings(const player_map&, 
                 player_map&);
             static std::vector<std::unique_ptr<nc_controller::command>>
                 create_actions(const player_map&, player_map&);
+
             virtual ~player_selection_window(void){}
         };
     }
