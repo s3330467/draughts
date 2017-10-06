d9414d2145203e6a8710639072a77918ef6dd5f2
1wsx10 <1wsx10@gmail.com>
2017-10-06 14:02:30 +1100

    gave comments for the scripts i made

--- assignments/a2/rmcamel.sh
+++ assignments/a2/rmcamel.sh
@@ -6,6 +6,11 @@
 #with:
 #(first saved thing)_(second saved thing TOLOWER)
 
+#ie, this:
+#myDirtyCamel
+#into this:
+#my_dirty_camel
+
 for arg in "$@"; do
 	sed -i 's|\([a-z]\)\([A-Z]\)|\1_\L\2|g' "$arg"
 done
