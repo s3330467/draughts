feff9d051e47b1969fddd122c5f051d6d3af65eb
Alexander Young <s3330467@csitprdap02.int.its.rmit.edu.au>
2017-10-15 00:08:28 +1100

    first version of available_moves method

--- assignments/a2/model/game_board.cpp
+++ assignments/a2/model/game_board.cpp
@@ -53,3 +53,48 @@ int game_board::make_move(coordinate from, coordinate to) {
 	}
 	return EOF;
 }
+
+std::vector<std::pair<coordinate,coordinate>> game_board::available_moves(int player_id) {
+	std::vector<move> available_moves;
+	std::vector<move> piece_moves;
+	game_piece current_piece, capture_piece;
+	coordinate capturecoords;
+	bool attackmoveexists = false;
+	for(unsigned int i = 0; i < board.size(); i++) {//itterate through board in x direciton
+
+		for(unsigned int j = 0; j < board[i].size(); j++) {//itterate through board in y direction (the column we just added)
+			coordinate coord = coordinate::from_crush(i, j);
+			current_piece = get_piece(coord);
+			piece_moves = current_piece.get_valid_moves();
+
+			//TODO check which player is having their moves checks, right now it checks all the pieces
+
+			for(unsigned int k = 0; k < piece_moves.size(); k++){
+				std::pair<int, int> fromcoords = piece_moves.get(k).get_from().get_uncrush();
+				std::pair<int, int> tocoords = piece_moves.get(k).get_to().get_uncrush();
+				int dx = tocoords.first - fromcoords.first;
+				int dy = tocoords.second - fromcoords.second;
+				if(piece_moves.get(k).get_move_type()=="attack"){
+					attackmoveexists = true;
+					capturedx = dx/2
+					capturedy = dy/2
+					capturecoords = coordinate.from_uncrush((fromcoords.first-capturedx),(fromcoords.second-capturedy));
+					capture_piece = get_piece(capturecoords);
+					if(get_piece(piece_moves.get(k).get_to()) != nullptr) continue;
+					if(capture_piece = nullptr) continue;
+					if(capture_piece.get_is_top() == current_piece.get_is_top()) continue;
+					//available_moves.reserve( available_moves.size() + piece_moves.size());
+					available_moves.push_back(piece_moves.get(k));
+				}
+				if(piece_moves.get(k).get_move_type()=="move" && attackmoveexists = false){
+					if(get_piece(piece_moves.get(k).get_to()) != nullptr) continue;
+					available_moves.push_back(piece_moves.get(k));
+				}
+			}
+		}
+	}
+	if(attackmoveexists = false){
+		//TODO remove any non-attack moves that were added prior to the attack being found
+	}
+	return available_moves;
+}
